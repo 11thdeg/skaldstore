40e7bb00ca529164a9d10d44f90881830939fc69
Ville Takanen <vitkukissa@gmail.com>
2021-12-30 20:59:51 +0200

    feat: the profile class added

---
+++ src/Profile.ts
@@ -0,0 +1,36 @@
+import type { DocumentData } from 'firebase/firestore'
+import { Entry } from './Entry'
+
+/**
+ * A Public Profile of a user.
+ */
+export class Profile extends Entry {
+  protected _nickname: string | undefined
+  protected _avatarURL: string | undefined
+  constructor(profile: string | DocumentData) {
+    super(profile)
+    if (typeof profile === 'string') {
+      this._id = profile
+    } else {
+      this._id = profile.id || ''
+      this.docData = profile
+    }
+  }
+  get nickname(): string {
+    return this._nickname || ''
+  }
+  get avatarURL(): string {
+    return this._avatarURL || ''
+  }
+  get docData(): DocumentData {
+    const data = super.docData
+    data.nickname = this._nickname
+    data.avatarURL = this._avatarURL
+    return data
+  }
+  set docData(data: DocumentData) {
+    super.docData = data
+    this._nickname = data.nickname || ''
+    this._avatarURL = data.avatarURL || ''
+  }
+}
---
+++ src/__tests__/Profile.spec.ts
@@ -0,0 +1,19 @@
+import { DocumentData, Timestamp } from 'firebase/firestore'
+import { Profile } from '../Profile'
+
+describe(
+  'Profile', () => {
+    it('Should hydrate Avatar URL and Nickname from docData', () => {
+      const ts = Timestamp.now()
+      const doc = {
+        id: 'test for docData',
+        avatarURL: 'https://avatar.com',
+        nickname: 'nickname',
+        createdAt: ts
+      } as DocumentData
+      const profile = new Profile(doc)
+      expect(profile.avatarURL).toBe('https://avatar.com')
+      expect(profile.nickname).toBe('nickname')
+      expect(profile.createdAt).toBe(ts)
+    })
+  })
