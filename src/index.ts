9e435936481eec573379500697eb6687b9a11dd4
Ville Takanen <vitkukissa@gmail.com>
2022-01-31 13:52:38 +0200

    fix: thread release failed

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,12 @@ import { Entry } from './Entry'
 import { Profile } from './entries/Profile'
 import { Account } from './Account'
 import { Asset } from './entries/Asset'
+import { Thread } from './entries/Thread'
 
-export { Account, Asset, Entry, Profile }
+export {
+  Account,
+  Asset,
+  Entry,
+  Profile,
+  Thread
+}
