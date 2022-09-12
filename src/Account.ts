2e4831fe278714c7b5582a8f874d349102767a27
Ville Takanen <ville.takanen@futurice.com>
2022-09-12 14:49:11 +0300

    fix: account now supports watches

--- src/Account.ts
+++ src/Account.ts
@@ -1,6 +1,11 @@
-import { serverTimestamp, Timestamp } from '@firebase/firestore'
+import { serverTimestamp, Timestamp, FieldValue } from '@firebase/firestore'
 import type { DocumentData } from '@firebase/firestore'
 import type { User } from '@firebase/auth'
+
+export type watchEntry = {
+  key: string
+  lastSeen: Timestamp|FieldValue
+}
 
 export class Account {
   email: string
@@ -12,6 +17,8 @@ export class Account {
   lightMode: string = ''
   updatedAt: Timestamp | null = null
   locale: string = 'en'
+  watched: watchEntry[] = []
+  seenSince: Timestamp | null = null
 
   constructor(user: User | DocumentData | null) {
     this.email = user?.email || ''
@@ -22,6 +29,12 @@ export class Account {
     this.providerId = user?.providerId || ''
     if (user && Object.keys(user).includes('lightMode')) {
       this.lightMode = (user as DocumentData).lightMode
+    }
+    if (user && Object.keys(user).includes('watched')) {
+      this.watched = (user as DocumentData).watched as watchEntry[]
+    }
+    if (user && Object.keys(user).includes('seenSince')) {
+      this.seenSince = (user as DocumentData).seenSince
     }
   }
 
@@ -37,8 +50,10 @@ export class Account {
     const data = {} as DocumentData
     data.uid = this.uid
     data.updatedAt = serverTimestamp()
-    data.lightMode = this.lightMode
-    data.locale = this.locale
+    data.lightMode = this.lightMode || 'light'
+    data.locale = this.locale || 'en'
+    if (this.watched && this.watched.length > 0) data.watched = this.watched
+    if (this.seenSince) data.seenSince = this.seenSince
     return data
   }
 
@@ -48,8 +63,10 @@ export class Account {
   set docData(data: DocumentData) {
     // uid is never set
     this.updatedAt = data.updatedAt || null
-    this.lightMode = data.lightMode || ''
+    this.lightMode = data.lightMode || 'light'
     this.locale = data.locale || 'en'
+    this.watched = data.watched || []
+    this.seenSince = data.seenSince || null
   }
 
   public static get collectionName(): string {
